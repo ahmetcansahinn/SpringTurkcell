ec55d4a8fdccd947729a59ec7f7a14f850e8f7f9
ahmetcansahinn <cannahmt@gmail.com>
2023-10-07 19:28:43 +0300

    Revert "OrderDetailService ve imp eklendi"
    This reverts commit 16ed3abb2d0e6e27ee5804aa37727103219288c7.

--- src/main/java/com/turkcell/spring/starter/repositories/OrderDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.turkcell.spring.starter.repositories;
-
-import com.turkcell.spring.starter.entities.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderDetailRepository extends JpaRepository<OrderDetail,Integer> {
-}
